9cc26274881c235c0df550b757b9d7d657ad9e6f
nickvazz <nickvazquezz@gmail.com>
2020-12-09 00:10:12 -0800

    update app to have header

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
+st.set_page_config('Tax Brackets from 1913-2020', page_icon=":money_with_wings:", layout="wide")
+
 f  = 'fed_individual_rate_history_nominal_1913_2020.csv'
 
 st.write(pd.read_csv(f, index_col=0))
